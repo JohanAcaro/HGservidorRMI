7e9a42d4f2914e41396072e8dcdc571ef53061fb
JohanAcaro <johan.acaro@campusfp.es>
2023-02-07 19:27:45 +0100

    Update sql

--- juego_tronos.sql
+++ juego_tronos.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE juego_tronos;
+# noinspection SqlNoDataSourceInspectionForFile
 
-USE juego_tronos;
+CREATE DATABASE juego_tronos;
 
 CREATE TABLE casa (
     ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,13 +20,13 @@ CREATE TABLE personaje (
 
 INSERT INTO casa (Nombre, Lema, Escudo)
 VALUES
-    ("Stark", "Se acerca el invierno", "Lobo huargo en un campo de nieve"),
-    ("Targaryen", "Fuego y sangre", "Dragones tres en un campo de rojo"),
-    ("Lannister", "¡Oye mi Rugido!", "León dorado en un campo de gules"),
+    ('Stark', 'Se acerca el invierno', 'Lobo huargo en un campo de nieve'),
+    ('Targaryen', 'Fuego y sangre', 'Dragones tres en un campo de rojo'),
+    ('Lannister', '¡Oye mi Rugido!', 'León dorado en un campo de gules'),
     ('Snow', 'Nunca muera', 'Wolf blanco sobre fondo gris'),
     ('Baratheon', 'Nuestra es la Furia', 'León Rampante'),
     ('Greyjoy', 'Nosotros no sembramos', 'Kraken'),
-    ("Mormont", "Aquí Aguantamos", "Oso"),
+    ('Mormont', 'Aquí Aguantamos', 'Oso'),
     ('Baelish', 'El Conocimiento es poder', 'Venado'),
     ('Seaworth', 'Honor, Deber, Disciplina', 'Barco');
     
@@ -35,9 +35,9 @@ VALUES
 
 INSERT INTO personaje (Nombre, Casa, Edad, Titulo)
 VALUES
-    ("Ned Stark", 1, 35, "Señor de Invernalia"),
-    ("Daenerys Targaryen", 2, 20, "Madre de Dragones"),
-    ("Tyrion Lannister", 3, 40, "Mano del Rey"),
+    ('Ned Stark', 1, 35, 'Señor de Invernalia'),
+    ('Daenerys Targaryen', 2, 20, 'Madre de Dragones'),
+    ('Tyrion Lannister', 3, 40, 'Mano del Rey'),
     ('Jon Snow', 4, 25, 'Rey en el Norte'),
     ('Arya Stark', 1, 17, 'Asesina'),
     ('Bran Stark', 1, 18, 'Brujo de la Cuarta'),
